1e042612f6cebf94eccfabed4f271b6bedfc8a74
Edward Townend <ed@dmsqd.com>
2015-08-26 10:54:44 +0100

    Tidying

--- bp-whats-hot.php
+++ bp-whats-hot.php
@@ -1,10 +1,10 @@
 <?php
 /*
-Plugin Name: What's Hot Activity Tab for Buddypress
+Plugin Name: What's Hot Activity Tab for BuddyPress
 Plugin URI: http://dmsqd.com
-Description: Adds a What's Hot Tab to the Activity Stream in Buddypress
+Description: Adds a What's Hot Tab to the Activity Stream in BuddyPress.
 Author: DMSQD
-Version: 0.1
+Version: 0.2
 Author URI: http://dmsqd.com
 Text Domain: bp-whats-hot
 */
@@ -12,20 +12,23 @@ Text Domain: bp-whats-hot
 /**
  * Create the tab
  */
-add_action( 'bp_before_activity_type_tab_friends', function() {
-?>
-            <li class="" id="activity-whats-hot">
-                <a href="<?php bp_activity_directory_permalink(); ?>" title="<?php esc_attr_e( 'The top content', 'bp-whats-hot' ); ?>"><?php printf( __( "What's Hot", 'bp-whats-hot' )); ?></a>
-            </li>
-<?php
-});
+
+function bp_whats_hot_add_tab() {
+    ?>
+    <li class="" id="activity-whats-hot">
+        <a href="<?php bp_activity_directory_permalink(); ?>" title="<?php esc_attr_e( 'The top content', 'bp-whats-hot' ); ?>"><?php printf( __( "What's Hot", 'bp-whats-hot' )); ?></a>
+    </li>
+    <?php
+}
+add_action( 'bp_before_activity_type_tab_friends', 'bp_whats_hot_add_tab');
 
 /**
  * Amend the query
  */
-add_filter('bp_activity_paged_activities_sql', function($activity_ids_sql) {
 
-    // check scope is whats-hot
+function bp_whats_hot_amend_query($activity_ids_sql) {
+
+    // Check scope is whats-hot
     if ( false === strpos(bp_ajax_querystring('activity'), 'scope=whats-hot') ) {
         return $activity_ids_sql;
     }
@@ -40,7 +43,12 @@ add_filter('bp_activity_paged_activities_sql', function($activity_ids_sql) {
         . substr($activity_ids_sql, $where_pos);
 
     // Alter ORDER BY
-    $activity_ids_sql = preg_replace('/ORDER BY a.date_recorded (DESC|ASC)/', 'ORDER BY GREATEST(c.date_recorded, a.date_recorded) DESC', $activity_ids_sql);
+    $activity_ids_sql = preg_replace(
+        '/ORDER BY a.date_recorded (DESC|ASC)/',
+        'ORDER BY GREATEST(c.date_recorded, a.date_recorded) DESC',
+        $activity_ids_sql
+    );
 
     return $activity_ids_sql;
-});
+}
+add_filter('bp_activity_paged_activities_sql', 'bp_whats_hot_amend_query');
